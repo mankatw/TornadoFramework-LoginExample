e1f838656997535cd009d1faa405c20fa589e16f
Bruno Farina <bruno.brumetal@gmail.com>
2013-08-07 17:20:31 -0300

    Improviments
    1st change:
    before :
    >>class SignupHandler(tornado.web.RequestHandler):
    now:
    >>class SignupHandler(BaseHandler):
    because now is used a class(inheritance concpet):
    >>class BaseHandler(tornado.web.RequestHandler):
    
    2nd change:
    separated a function, in other classe, because this make a clear code;
    
    3rd change:
    separate the settings and handlers from the application main function;

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import cgi
+import os
 import pymongo
-import re
+import utils
 import sessionDAO
-import tornado.ioloop
 import tornado.web
 import userDAO
 
@@ -15,60 +15,42 @@ database = connection.blog
 users = userDAO.UserDAO(database)
 sessions = sessionDAO.SessionDAO(database)
 
-def validate_signup(username, password, verify, email, errors):
-    USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-    PASS_RE = re.compile(r"^.{3,20}$")
-    EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return 
+       
+    def post_current_user(self):
+        return 
 
-    errors['username_error'] = ""
-    errors['password_error'] = ""
-    errors['verify_error'] = ""
-    errors['email_error'] = ""
-
-    if not USER_RE.match(username):
-        errors['username_error'] = "invalid username. try just letters and numbers"
-        return False
-
-    if not PASS_RE.match(password):
-        errors['password_error'] = "invalid password."
-        return False
-    if password != verify:
-        errors['verify_error'] = "password must match"
-        return False
-    if email != "":
-        if not EMAIL_RE.match(email):
-            errors['email_error'] = "invalid email address"
-            return False
-    return True
-
-class IndexHandler(tornado.web.RequestHandler):
+class IndexHandler(BaseHandler):
     def get(self):
         cookie = self.get_cookie("session")
         username = sessions.get_username(cookie)
-        self.render('templates/blog_template.html', username=username)      
+        self.render('blog_template.html', username = username)      
     
-class SignupHandler(tornado.web.RequestHandler):
+class SignupHandler(BaseHandler):
     def get(self):
-        return self.render("templates/signup.html",
+        return self.render("signup.html",
                            username="", password="",
                            password_error="",
                            email="", username_error="", email_error="",
                            verify_error="")
         
     def post(self):
-        email = self.get_argument("email")
+        email    = self.get_argument("email")
         username = self.get_argument("username")
         password = self.get_argument("password")
-        verify = self.get_argument("verify")
+        verify   = self.get_argument("verify")
     
         # set these up in case we have an error case
         errors = {'username': cgi.escape(username), 'email': cgi.escape(email)}
-        if validate_signup(username, password, verify, email, errors):
+        
+        if utils.validate_signup(username, password, verify, email, errors):
     
             if not users.add_user(username, password, email):
                 # this was a duplicate
                 errors['username_error'] = "Username already in use. Please choose another"
-                return self.render("views/signup", errors)
+                return self.render("signup.html", errors)
     
             session_id = sessions.start_session(username)
             print (session_id)
@@ -76,39 +58,36 @@ class SignupHandler(tornado.web.RequestHandler):
             self.redirect("/welcome")
         else:
             print ("user did not validate")
-            return self.render("templates/signup.html", errors)
+            return self.render("signup.html", errors)
 
-class LoginHandler(tornado.web.RequestHandler):
+class LoginHandler(BaseHandler):
     def get(self):
-        self.render('templates/login.html', username="", password="", login_error="")
+        self.render('login.html', username="", password="", login_error="")
         
     def post(self):
         username = self.get_argument("username")
         password = self.get_argument("password")
     
-        print ("user submitted ", username, "pass ", password)
+        print ("user submitted: ", username, "| pass: ", password)
     
         user_record = users.validate_login(username, password)
         if user_record:
+
             # username is stored in the user collection in the _id key
             session_id = sessions.start_session(user_record['_id'])
     
             if session_id is None:
                 self.redirect("/internal_error")
-    
             cookie = session_id
-
             self.set_cookie("session", cookie)
-    
             self.redirect("/welcome")
-    
         else:
-            return self.render("templates/login.html",
+            return self.render("login.html",
                                username=cgi.escape(username),
                                password="",
                                login_error="Invalid Login")     
 
-class WelcomeHandler(tornado.web.RequestHandler):
+class WelcomeHandler(BaseHandler):
     def get(self):
         # check for a cookie, if present, then extract value
     
@@ -116,32 +95,39 @@ class WelcomeHandler(tornado.web.RequestHandler):
         username = sessions.get_username(cookie)  # see if user is logged in
         if username is None:
             print ("welcome: can't identify user...redirecting to signup")
-            self.redirect("templates/signup.html")
+            self.redirect("signup.html")
     
-        return self.render("templates/welcome.html", username=username)  
+        return self.render("welcome.html", username=username)  
     
-class LogoutHandler(tornado.web.RequestHandler):
+class LogoutHandler(BaseHandler):
     def get(self):
         cookie = self.get_cookie("session")
         sessions.end_session(cookie)
         self.set_cookie("session", "")
         self.redirect("/signup")
 
-class InternalError(tornado.web.RequestHandler):
+class ErrorBaseHandler(tornado.web.ErrorHandler):
+    def get_error(self):
+        return tornado.escape.json_decode("System has encountered a DB error") 
+
+class InternalError(ErrorBaseHandler):
     def get(self):
-        return self.render("templates/error_template.html", error ="System has encountered a DB error")        
+        return self.render("error_template.html", ErrorBaseHandler.get_error(self))  
+    
+settings = {
+    "template_path": os.path.join(os.path.dirname(__file__), "templates")
+}
 
-def main():
-    application = tornado.web.Application([
-            (r"/", IndexHandler),
-            (r"/login", LoginHandler),
-            (r"/signup", SignupHandler),
-            (r"/welcome", WelcomeHandler),
-            (r"/logout", LogoutHandler),
-            (r"/internalerror", InternalError),
-    ])
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+handlers = [
+        (r"/", IndexHandler),
+        (r"/login", LoginHandler),
+        (r"/signup", SignupHandler),
+        (r"/welcome", WelcomeHandler),
+        (r"/logout", LogoutHandler),
+        (r"/internal_error", InternalError)]
 
-if __name__ == "__main__":
-    main()
+
+application = tornado.web.Application(handlers , **settings)
+
+application.listen(8888)
+tornado.ioloop.IOLoop.instance().start()
